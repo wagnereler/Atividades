7cc9b9332db0ce273ea78f55a25b1825267ef6c1
wagne <wagne@JANE-PC>
2017-03-04 14:49:07 -0400

    ver. 0.065

--- Atividades/Atividades/DataConnect.cs
+++ Atividades/Atividades/DataConnect.cs
@@ -88,7 +88,11 @@ CREATE TABLE [tbDeslocamento](
     [codCidadeDestino] INT, 
     [obsDestino] VARCHAR(100), 
     [codProjeto] INT, 
-    [dataDeslocamento] VARCHAR(10));");
+    [dataDeslocamento] VARCHAR(10), 
+    [horaInicio] NVARCHAR(5), 
+    [horaFim] VARCHAR(5), 
+    [totalMinutosDeslocamento] INT, 
+    [minutosExtrasDeslocamento] INT);");
 
 
 
